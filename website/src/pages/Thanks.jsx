d2d34871bd15b171af46805a56e842f601050e28
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-06-01 02:22:10 +0530

    thanks page ready

--- website/src/pages/Thanks.jsx
+++ website/src/pages/Thanks.jsx
@@ -1,7 +1,45 @@
+import { Navbar } from "../components/Navbar";
+
 export var ThanksPage = () => {
   return (
     <>
-      <h1 className="p-5 text-4xl">thanks</h1>
+      <Navbar />
+      <h1 className="text-4xl p-10">A sincere thanks from the lead to</h1>
+      <div id="content" className="flex flex-col gap-5 p-10">
+        <div className="person">
+          <h2 className="text-2xl">Madam Mamta Panwar</h2>
+          <p className="text-lg px-5">
+            for empowering us to work on this project.{" "}
+          </p>
+        </div>
+        <div className="person">
+          <h2 className="text-2xl">Principal Madam Kamna Beri</h2>
+          <p className="text-lg px-5">
+            for not only allowing us work on this project but also to shoot a
+            short film for the same
+          </p>
+        </div>
+        <div className="person">
+          <h2 className="text-2xl">Master Dhiman and Master Bhatt</h2>
+          <p className="text-lg px-5">
+            for working with me and trusting me to lead this amazing team
+            forward.
+          </p>
+        </div>
+        <div className="person">
+          <h2 className="text-2xl">Master Mandhotra</h2>
+          <p className="text-lg px-5">
+            for supplying all critical hardware supplies we needed during the
+            tenure of the project and aiding in filming.
+          </p>
+        </div>
+
+        <div className="person">
+          <p className="text-4xl text-center p-8 font-bold">
+            Thanks for the awesome time.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
